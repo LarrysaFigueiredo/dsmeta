92f8bda4f07e94bcd1d55f5559ae06d0655fe198
LarrysaFigueiredo <larrysa.figueiredo@gmail.com>
2022-09-11 21:20:49 -0300

    comp:First

--- frontend/src/assets/components/NotificationButton/index.tsx
+++ frontend/src/assets/components/NotificationButton/index.tsx
@@ -1,6 +1,11 @@
+import icon from '../../img/notification-icon.svg';
+import './style.css'
+
 function NotificationButton() {
     return(
-        <h1> </h1>
+        <div className="dsmeta-red-btn">
+            <img src={icon} alt="Notificar" />
+        </div>
     )
 }
 
